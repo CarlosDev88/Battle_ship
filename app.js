ab3d6c81e1296c42181881e1db1b859cc3f94877
Carlos <arturorueda88@gmail.com>
2021-06-27 16:36:30 -0500

    creando el modelo

---
+++ app.js
@@ -0,0 +1,102 @@
+let model = {
+  boardSize: 7,
+  numShip: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+  ],
+
+  fire: function (guess) {
+    for (let i = 0; i < this.numShip; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+
+      if (ship.hits[index] === "hit") {
+        view.displayMessage("you have already hit this location!");
+        return true;
+      } else if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battle ship");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed!");
+    return false;
+  },
+
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  generateShipLocations: function () {
+    let locations;
+
+    for (let i = 0; i < this.numShip; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+
+      this.ships[i].locations = locations;
+    }
+
+    console.log("ships array: ", this.ships);
+  },
+
+  generateShip: function () {
+    let direction = Math.floor(Math.random() * 2);
+    let row;
+    let col;
+
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    } else {
+      col = Math.floor(Math.random() * this.boardSize);
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    }
+
+    let newShipLocations = [];
+    for (let i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+
+  collision: function (location) {
+    for (let i = 0; i < this.numShip; i++) {
+      let ship = this.ships[i];
+      for (let j = 0; j < location.length; j++) {
+        if (ship.locations.indexOf(location[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+};
+
+let view = {
+  displayMessage: function (message) {},
+  displayHit: function (guess) {},
+  displayMiss: function (guess) {},
+};
